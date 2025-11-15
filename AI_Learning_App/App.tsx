67750ddfbfea819fbe14676e44763a317e07b9b4
kontee809 <congti1812@gmail.com>
2025-11-15 10:59:23 +0700

    first commit

---
+++ AI_Learning_App/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, useColorScheme, StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello Wordsdfsdsld2đâsdas</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',     
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
